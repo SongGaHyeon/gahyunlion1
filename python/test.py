f074989219ab68b7b9cb2bdfd4e2713025afc8f4
SongGaHyeon <gahyunsong@hufs.ac.kr>
2021-05-13 01:28:43 +0900

    python 1이론

---
+++ python/test.py
@@ -0,0 +1,5 @@
+number1 = int(input("숫자1을 입력하세요"))
+number2 = int(input("숫자2를 입력하세요"))
+
+print(number1+number2)
+
---
+++ python/자료형.py
@@ -0,0 +1,18 @@
+#주석 = 메모
+#print("안녕")
+
+#실수형 float
+#정수형 int
+
+#num1 = int(input())
+#num2 = int(input())
+#print(num1+num2)
+#print(num1*num2)
+
+#나눗셈 중요
+#몫 //
+#나머지 %
+#몫+나머지:/
+
+#인덱싱,슬라이싱
+
